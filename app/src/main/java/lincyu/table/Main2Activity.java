760fcc99fa3dfd1e5e4f49208fbd583b1d51bb3d
0+7 <yoyo40204@yahoo.com.tw>
2016-04-08 14:43:14 +0800

    new

---
+++ app/src/main/java/lincyu/table/Main2Activity.java
@@ -0,0 +1,26 @@
+package lincyu.table;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ImageView;
+
+public class Main2Activity extends AppCompatActivity {
+    private ImageView ImageView_view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        ImageView_view = (ImageView) findViewById(R.id.View);
+        ImageView_view.setImageResource(R.drawable.th);
+
+    }
+
+}
---
+++ app/src/main/java/lincyu/table/MainActivity.java
@@ -0,0 +1,72 @@
+package lincyu.table;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button BT_connect;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        BT_connect =(Button)findViewById(R.id.buttonConnect);
+        BT_connect.setOnClickListener(connectListener);
+
+
+    }
+    private View.OnClickListener connectListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick (View v) {
+            Intent intent = new Intent();
+            intent.setClass(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_use:
+
+
+                break;
+            case R.id.action_exit:
+
+
+                break;
+
+
+        }
+
+        //noinspection SimplifiableIfStatement
+
+
+        return super.onOptionsItemSelected(item);
+    }
+}
